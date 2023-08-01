36f7361de4d715a0969a235299b4ac30379babe5
unknown <bb3583400@gmail.com>
2023-08-02 01:00:46 +0600

    pagea added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+          <div className="">@2023 Babul Kumar All right reserved</div>
+          <div className=""></div>
+        </div>
+    );
+};
+
+export default Footer;
