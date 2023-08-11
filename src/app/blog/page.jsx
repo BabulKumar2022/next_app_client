275956a9ba0d1f92543c90a64e1f6cafb2524d0e
unknown <bb3583400@gmail.com>
2023-08-12 00:50:11 +0600

    blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,29 +4,31 @@ import styles from './page.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
-import Error from 'next/error';
+// import Error from 'next/error';
 
 async function getData(){
     const res = await fetch("http://localhost:3000/api/posts",{
         cache: "no-store"
     });
-    
+    console.log(res)
     if(!res.ok){
-        throw new Error("Failed to fetch data")
-        // return notFound()
+        // throw new Error("Failed to fetch data")
+        return notFound()
     }
     return res.json();
+    
 }
 
 const Blog = async () => {
     
     const data = await getData();
+    console.log(data)
     return (
         <div className={styles.mainContainer}>
             {
                 data.map((item) =>(
+
                 <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
-                        
                         <div className={styles.imageContainer}>
                             <Image  className={styles.image} src={item.image}   alt="" width={400} height={250}/>
                         </div>
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const Dashboard = () => {
     // const [data, setData] = useState([]);
@@ -27,10 +28,14 @@ const Dashboard = () => {
     const session = useSession();
     console.log(session)
     const router = useRouter();
-    const fetcher = (...args) => fetch(...args).then(res => res.json());
-    const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher)
-   
-    console.log(data)
+    const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/posts?username=${session?.data?.user.name}`,
+    fetcher
+  );
+
+
 
     if(session.status === "loading"){
         return <p>Loading........</p>;
@@ -38,12 +43,78 @@ const Dashboard = () => {
     if(session.status === "unauthenticated"){
         return router?.push("/dashboard/login")
     }
+//------------------------
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const image = e.target[2].value;
+    const content = e.target[3].value;
 
-   
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          desc,
+          image,
+          content,
+          username: session.data.user.name,
+        }),
+      });
+      mutate();
+      e.target.reset()
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  //----------------delete-----------------
+  const handleDelete = async (id) => {
+    try {
+      await fetch(`/api/posts/${id}`, {
+        method: "DELETE",
+      });
+      mutate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(data)
     if(session.status === "authenticated"){
+        // console.log(data)
         return (
             <div className={styles.container}>
-                dashboard
+         
+                <div className={styles.posts}>
+                    {isLoading
+                        ? "loading"
+                       : data?.map((post) =>(
+                            <div className={styles.post} key={post?._id}>
+                                <div className={styles.imageContainer}>
+                                    <Image src={post.image} alt="" />
+                                </div>
+                                <h2 className={styles.postTitle}>{post?.title}</h2>
+                                <span  className={styles.delete}
+                                onClick={() => handleDelete(post._id)}>X</span>
+                            </div>
+                        ))
+                    }
+
+                </div>
+                <form className={styles.new} onSubmit={handleSubmit}>
+                         <h1>Add New Post</h1>
+                    <input type="text" placeholder="Title" className={styles.input} />
+                    <input type="text" placeholder="Desc" className={styles.input} />
+                    <input type="text" placeholder="Image" className={styles.input} />
+                    <textarea
+                        placeholder="Content"
+                        className={styles.textArea}
+                        cols="30"
+                        rows="10"
+                    ></textarea>
+                    <button className={styles.button}>Send</button>
+                </form>
             </div>
         );
   
